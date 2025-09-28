b2457e50a22b57de7d9726fc832dbc5731b9abe4
DatNT <datnguyen151004@gmail.com>
2025-09-28 17:58:00 +0700

    feat(Word) add CRUD word

---
+++ backend/src/routes/word.route.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const { Router } = require("express");
+const wordController = require("../controllers/word.controller");
+const { asynchandler } = require("../helpers/asyncHandler");
+const auth = require("../middlewares/auth");
+const { validateCreateWord } = require("../middlewares/word");
+const upload = require("../middlewares/upload");
+
+const router = Router();
+router.post("/create", auth.authenticate, validateCreateWord, asynchandler(wordController.createWord));
+router.get("/category/:categoryId", asynchandler(wordController.getWordsByCategory));
+router.put("/:wordId", auth.authenticate, validateCreateWord, asynchandler(wordController.updateWord));
+router.delete("/delete/:wordId", auth.authenticate, asynchandler(wordController.deleteWord));
+router.post("/import", auth.authenticate, upload.single("file"), asynchandler(wordController.importWords));
+router.get("/export-sample", auth.authenticate, asynchandler(wordController.exportSampleWords));
+module.exports = router;
