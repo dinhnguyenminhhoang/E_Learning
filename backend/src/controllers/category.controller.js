7ee1969be790383c1eb27b9151883d8a71260e3c
Duc Quy <luongducquy20041117@gmail.com>
2025-10-04 16:01:40 +0700

    [add]:check-api

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -3,92 +3,33 @@
 const categoryService = require("../services/category.service");
 
 class CategoryController {
-  // POST /v1/api/category
-  async createCategory(req, res, next) {
-    try {
-      const category = await categoryService.createCategory(req.body);
-
-      if (category.status === "error") {
-        return res.status(category.code).json(category);
-      }
-
-      return res.status(201).json({
-        metadata: category,
-      });
-    } catch (error) {
-      next(error);
-    }
+  
+  async createCategory(req, res) {
+    const category = await categoryService.createCategory(req.body);
+    return res.status(category.code).json(category);
   }
 
-  // GET /v1/api/category/search?q=keyword&limit=20&skip=0
-  async listCategories(req, res, next) {
-    try {
-      const categories = await categoryService.findAllCategories(req.query);
-
-      if (categories.status === "error") {
-        return res.status(categories.code).json(categories);
-      }
-
-      return res.json({
-        metadata: categories,
-      });
-    } catch (error) {
-      next(error);
-    }
+  async listCategories(req, res) {
+    const categories = await categoryService.findAllCategories(req.query);
+    return res.status(categories.code).json(categories);
   }
 
-  // GET /v1/api/category/:id
-  async getCategoryById(req, res, next) {
-    try {
-      const category = await categoryService.getCategoryById(req.params.id);
-
-      if (category.status === "error") {
-        return res.status(category.code).json(category);
-      }
-
-      return res.json({
-        metadata: category,
-      });
-    } catch (error) {
-      next(error);
-    }
+  async getCategoryById(req, res) {
+    const category = await categoryService.getCategoryById(req.params.id);
+    return res.status(category.code).json(category);
   }
 
-  // PUT /v1/api/category/:id
-  async updateCategory(req, res, next) {
-    try {
-      const category = await categoryService.updateCategory(
-        req.params.id,
-        req.body
-      );
-
-      if (category.status === "error") {
-        return res.status(category.code).json(category);
-      }
-
-      return res.json({
-        metadata: category,
-      });
-    } catch (error) {
-      next(error);
-    }
+  async updateCategory(req, res) {
+    const category = await categoryService.updateCategory(
+      req.params.id,
+      req.body
+    );
+    return res.status(category.code).json(category);
   }
 
-  // DELETE /v1/api/category/:id
   async deleteCategory(req, res, next) {
-    try {
-      const result = await categoryService.deleteCategory(req.params.id);
-
-      if (result.status === "error") {
-        return res.status(result.code || 400).json(result);
-      }
-
-      return res.json({
-        metadata: result,
-      });
-    } catch (error) {
-      next(error);
-    }
+    const result = await categoryService.deleteCategory(req.params.id);
+    return res.status(result.code).json(result);
   }
 }
 
--- backend/src/controllers/flashCard.controller.js
+++ backend/src/controllers/flashCard.controller.js
@@ -3,97 +3,41 @@
 const flashcardService = require("../services/flashCard.service");
 
 class FlashcardController {
-  async create(req, res, next) {
-    try {
-      const flashcard = await flashcardService.createFlashcard(req.body);
-
-      if (flashcard.status === "error") {
-        return res.status(flashcard.code).json(flashcard);
-      }
-
-      res.status(201).json({ metadata: flashcard });
-    } catch (error) {
-      next(error);
-    }
+  async create(req, res) {
+    const flashcard = await flashcardService.createFlashcard(req.body);
+    return res.status(flashcard.code).json(flashcard);
   }
 
-  async getOne(req, res, next) {
-    try {
-      const flashcard = await flashcardService.getFlashcardById(req.params.id);
-
-      if (flashcard.status === "error") {
-        return res.status(flashcard.code).json(flashcard);
-      }
-
-      res.json({ metadata: flashcard });
-    } catch (error) {
-      next(error);
-    }
+  async getOne(req, res) {
+    const flashcard = await flashcardService.getFlashcardById(req.params.id);
+    return res.status(flashcard.code).json(flashcard);
   }
 
-  async update(req, res, next) {
-    try {
-      const flashcard = await flashcardService.updateFlashcard(
-        req.params.id,
-        req.body
-      );
-
-      if (flashcard.status === "error") {
-        return res.status(flashcard.code).json(flashcard);
-      }
-
-      res.json({ metadata: flashcard });
-    } catch (error) {
-      next(error);
-    }
+  async update(req, res) {
+    const flashcard = await flashcardService.updateFlashcard(
+      req.params.id,
+      req.body
+    );
+    return res.status(flashcard.code).json(flashcard);
   }
 
-  async delete(req, res, next) {
-    try {
-      const flashcard = await flashcardService.deleteFlashcard(
-        req.params.id,
-      );
-
-      if (flashcard.status === "error") {
-        return res.status(flashcard.code).json(flashcard);
-      }
-
-      res.json({ metadata: flashcard });
-    } catch (error) {
-      next(error);
-    }
+  async delete(req, res) {
+    const flashcard = await flashcardService.deleteFlashcard(req.params.id);
+    return res.status(flashcard.code).json(flashcard);
   }
 
-  async list(req, res, next) {
-    try {
-      const flashcards = await flashcardService.listFlashcards({});
-
-      if (flashcards.status === "error") {
-        return res.status(flashcards.code).json(flashcards);
-      }
-
-      res.json({ metadata: flashcards });
-    } catch (error) {
-      next(error);
-    }
+  async list(res) {
+    const flashcards = await flashcardService.listFlashcards({});
+    return res.status(flashcards.code).json(flashcards);
   }
 
-  async search(req, res, next) {
-    try {
-      const { q, limit, skip } = req.query;
-      const flashcards = await flashcardService.searchFlashcards(q, {
-        limit: Number(limit) || 20,
-        skip: Number(skip) || 0,
-      });
-
-      if (flashcards.status === "error") {
-        return res.status(flashcards.code).json(flashcards);
-      }
-
-      res.json({ metadata: flashcards });
-    } catch (error) {
-      next(error);
-    }
+  async search(req, res) {
+    const { q, limit, skip } = req.query;
+    const flashcards = await flashcardService.searchFlashcards(q, {
+      limit: Number(limit) || 20,
+      skip: Number(skip) || 0,
+    });
+    return res.status(flashcards.code).json(flashcards);
   }
 }
 
