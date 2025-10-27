0355a5496b309ef56951340215c7a34542c59749
DatNT <datnguyen151004@gmail.com>
2025-10-27 19:43:28 +0700

    add feature lesson-block

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  const statusCode = error.status || 500;
+  const statusCode = error.code || 500;
   return res.status(statusCode).json({
     status: "error",
     code: statusCode,
