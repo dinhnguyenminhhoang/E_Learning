3fd15215536237bef7b6df5d773289da0121ca44
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2025-11-10 08:58:09 +0700

    [update]:redis production

--- backend/src/constants/blocks.js
+++ backend/src/constants/blocks.js
@@ -1,7 +1,7 @@
 const GrammarBlock = require("../models/subModel/grammarBlock.schema");
 const MediaBlock = require("../models/subModel/mediaBlock.schema");
 const QuizBlock = require("../models/subModel/quizBlock.schema");
-const VocabularyBlock = require("../models/subModel/vocabularyBlock.schema");
+const VocabularyBlock = require("../models/subModel/VocabularyBlock.schema");
 
 const BLOCK_MODELS = {
   grammar: GrammarBlock,
